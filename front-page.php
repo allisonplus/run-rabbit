afc521062826aa9e8b6036e39e25954a3e9689d8
Allison Tarr <hello@allisontarr.com>
2015-06-16 18:35:35 -0400

    front creation

---
+++ front-page.php
@@ -0,0 +1,19 @@
+<?php //front-page ?>
+<?php get_header(); ?>
+
+<div class="main">
+  <div class="container">
+
+    <div class="content">
+    		
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt ratione repellat aspernatur necessitatibus a vitae doloremque impedit, dignissimos fuga magni quo adipisci praesentium accusantium officiis, sit ducimus perspiciatis iusto eius. Esse expedita delectus, quis veniam neque officiis necessitatibus cum pariatur mollitia fugit saepe dolorum doloribus omnis rem sit minima commodi velit adipisci voluptas ex officia.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt ratione repellat aspernatur necessitatibus a vitae doloremque impedit, dignissimos fuga magni quo adipisci praesentium accusantium officiis, sit ducimus perspiciatis iusto eius. Esse expedita delectus, quis veniam neque officiis necessitatibus cum pariatur mollitia fugit saepe dolorum doloribus omnis rem sit minima commodi velit adipisci voluptas ex officia.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt ratione repellat aspernatur necessitatibus a vitae doloremque impedit, dignissimos fuga magni quo adipisci praesentium accusantium officiis, sit ducimus perspiciatis iusto eius. Esse expedita delectus, quis veniam neque officiis necessitatibus cum pariatur mollitia fugit saepe dolorum doloribus omnis rem sit minima commodi velit adipisci voluptas ex officia.</p>
+				
+
+    </div> <!--/.content -->
+
+  </div> <!-- /.container -->
+</div> <!-- /.main -->
+
+<?php get_footer(); ?>
