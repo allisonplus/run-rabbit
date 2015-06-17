82fa923a8d8f66d527dcdb7739ecdf782b4dd32a
Allison Tarr <hello@allisontarr.com>
2015-06-16 20:33:11 -0400

    toggle nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(function(){
 	//your jQuery here
 });
+
+$('a.menuIcon').on('click', function(){
+	$('.toggleNav').toggleClass('appear');
+	$('.main').toggleClass('showMenu');
+	// $('footer').toggleClass('showMenu');
+	// $('h1 a.title').toggleClass('showMenu');
+});
