5a7c9988327851cd7ee1076c3a8ea7cb724a76c3
Allison Tarr <hello@allisontarr.com>
2015-06-18 10:14:54 -0400

    about page social media icons

--- functions.php
+++ functions.php
@@ -13,6 +13,8 @@ function theme_setup() {
 	add_theme_support( 'post-thumbnails' );
 	set_post_thumbnail_size(120, 90, true);
 	add_image_size('square', 150, 150, true);
+	add_image_size('artist', 600, 348, true);
+
 
 
 	// Add default posts and comments RSS feed links to head
---
+++ page-about.php
@@ -0,0 +1,33 @@
+<?php get_header();  ?>
+
+<div class="main">
+  <div class="container">
+
+    <div class="content">
+
+			<div class="about-50">
+        <div class="bio">
+        <h2><?php the_title(); ?></h2>
+        	<p><?php the_field('artist_biography'); ?></p>
+        </div> <!--/.bio-->
+    	</div> <!--/.about-50-->
+
+			<div class="about-50">
+				<div class="photo clearfix">
+				    <?php $bio = get_field('artists_photo'); ?>
+				    <img src="<?php echo $bio['sizes']['artist']; ?> " alt="">
+				</div> <!--/.photo-->
+
+        <ul class="contact-icons">
+          <li><a href="<?php the_field('instagram'); ?>"><i class="fa fa-instagram"></i></a></li>
+          <li><a href="<?php the_field('linkedin'); ?>"><i class="fa fa-linkedin"></i></a></li>
+          <li><a href="mailto:<?php the_field('email'); ?>"><i class="fa fa-envelope-o"></i></a></li>
+        </ul> <!--/.contact-icons-->
+       </div> <!--/.about-50-->
+
+    </div> <!-- /,content -->
+
+  </div> <!-- /.container -->
+</div> <!-- /.main -->
+
+<?php get_footer(); ?>
