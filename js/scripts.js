8254b08048d30ffb3773c71bf6ccc404f4b44933
Allison Tarr <hello@allisontarr.com>
2015-07-29 17:00:21 -0400

    nav success exclamation point

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,32 @@
 
-// If nav isn't on the main page but it needs to scroll, I still need the first child to be display flex
-
-
 //Sticky Nav
 $(document).ready(function() {
-
-  //Calculate height
-  var totalHeight =$('.hero').outerHeight();
-
-  $(window).scroll(function() {
-    //if scrolled down more than the height of hero + stats
-    if ($(window).scrollTop() > totalHeight) {
-      $('nav').addClass('sticky');
-      $('ul.menu li:first-child').css({
-          "display":"flex"
-      });
-
-      } else {
-      // resetting nav when scrolled up
-      $('nav').removeClass('sticky');
-      $('ul.menu li:first-child').css({
-          "display":"none"
-      });
-    }; //end else statement
-  }); //end scroll function
-}) //end document ready
-
-// Nav Showing Logo + Home Link On Non-Home Pages
-$(function(){
   if ($("body").hasClass("home")){
-      // nav will act proper
-    } else {
-      $('ul.menu li:first-child').css({
-          "display":"flex"
-      });
-    }
-});
+
+    //Calculate height
+    var totalHeight =$('.hero').outerHeight();
+
+    $(window).scroll(function() {
+      //if scrolled down more than the height of hero + stats
+      if ($(window).scrollTop() > totalHeight) {
+        $('nav').addClass('sticky');
+        $('ul.menu li:first-child').css({
+            "display":"flex"
+        });
+
+        } else {
+        // resetting nav when scrolled up
+        $('nav').removeClass('sticky');
+        $('ul.menu li:first-child').css({
+            "display":"none"
+        });
+      }; //end else statement
+    }); //end window scroll function  
+
+  } else {
+    $('nav').addClass('sticky');
+    $('ul.menu li:first-child').css({
+        "display":"flex"
+    });
+  } //end else statement
+}); //end function
