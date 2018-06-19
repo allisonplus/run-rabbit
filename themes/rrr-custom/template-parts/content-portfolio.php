4131f7d6757ddfda785a03ba0f8136c43dff5493
Allison Tarr <hello@allisontarr.com>
2018-06-19 12:01:26 -0400

    ✏️Adjust single portfolio markup organization + post navigation

--- themes/rrr-custom/template-parts/content-portfolio.php
+++ themes/rrr-custom/template-parts/content-portfolio.php
@@ -12,34 +12,32 @@ $terms = wp_get_object_terms( $id, 'portfolio_category' );
 foreach ( $terms as $term ) {
 	$term_single = 'portfolio_category-' . $term->slug;
 }
-
 ?>
 
 <article <?php post_class(); ?> data-filter=".<?php echo esc_attr( $term_single ); ?>">
-	<header class="entry-header">
-		<?php
-		if ( has_post_thumbnail() ) {
-		    the_post_thumbnail( 'portfolio-archive' );
-		}
-		?>
-	</header><!-- .entry-header -->
+	<div class="portfolio-single-shell">
+		<div class="portfolio-featured-wrapper">
+			<?php echo rrr_get_portfolio_gallery(); // WPCS: XSS OK. ?>
+		</div>
 
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'rrr' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'rrr' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+		<div class="entry-content">
+			<?php
+				the_title( '<h2 class="entry-title">', '</h2>' );
+				the_content( sprintf(
+					/* translators: %s: Name of current post. */
+					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'rrr' ), array( 'span' => array( 'class' => array() ) ) ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				) );
+			?>
+		</div><!-- .entry-content -->
+	</div>
 
 	<footer class="entry-footer">
+		<?php
+		the_post_navigation( array(
+				'prev_text' => '<h4 class="pre-title">' . __( 'Previous: ', 'rrr' ) . '<span class="title">%title</span></h4>',
+				'next_text' => '<h4 class="pre-title">' . __( 'Next: ', 'rrr' ) . '<span class="title">%title</span></h4>',
+		) ); ?>
 		<?php rrr_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
