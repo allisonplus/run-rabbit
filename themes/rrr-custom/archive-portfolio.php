ccf67dd75e543014051b7dd3ceef22a539df90a6
Allison Tarr <hello@allisontarr.com>
2018-07-04 13:18:14 -0400

    ✨Coming Soon Portfolio section + assets

--- themes/rrr-custom/archive-portfolio.php
+++ themes/rrr-custom/archive-portfolio.php
@@ -20,10 +20,21 @@ get_header(); ?>
 					<?php
 						the_archive_title( '<h1 class="page-title">', '</h1>' );
 					?>
-				</header><!-- .page-header -->
+				</header><!--.page-header-->
 
+				<?php // get_template_part( 'template-parts/portfolio', 'filter' ); ?>
 
-				<?php get_template_part( 'template-parts/portfolio', 'filter' ); ?>
+				<section class="coming-soon">
+					<h2><?php esc_html_e( 'Coming Soon', 'rrr' ); ?></h2>
+					<img class="coming-soon" src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/rrr-coming-soon.jpg' ); ?>" alt="">
+
+					<div class="hex-container">
+						<div class="hex hex-one"></div>
+						<div class="hex hex-two"></div>
+						<div class="hex hex-three"></div>
+						<div class="hex hex-four"></div>
+					</div>
+				</section>
 
 				<div class="article-wrapper wrap">
 				<?php
@@ -35,7 +46,7 @@ get_header(); ?>
 					 * If you want to override this in a child theme, then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
-					get_template_part( 'template-parts/portfolio', 'archive' );
+					// get_template_part( 'template-parts/portfolio', 'archive' );
 
 				endwhile; ?>
 
