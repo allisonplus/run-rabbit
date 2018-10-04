e14941398bb5665975cea74300b44e296a2518cd
Allison Tarr <hello@allisontarr.com>
2018-10-03 20:34:13 -0400

    comment back in archive portfolio + filter functionality

--- themes/rrr-custom/archive-portfolio.php
+++ themes/rrr-custom/archive-portfolio.php
@@ -22,19 +22,7 @@ get_header(); ?>
 					?>
 				</header><!--.page-header-->
 
-				<?php // get_template_part( 'template-parts/portfolio', 'filter' ); ?>
-
-				<section class="coming-soon">
-					<h2><?php esc_html_e( 'Coming Soon', 'rrr' ); ?></h2>
-					<img class="coming-soon" src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/rrr-coming-soon.jpg' ); ?>" alt="">
-
-					<div class="hex-container">
-						<div class="hex hex-one"></div>
-						<div class="hex hex-two"></div>
-						<div class="hex hex-three"></div>
-						<div class="hex hex-four"></div>
-					</div>
-				</section>
+				<?php get_template_part( 'template-parts/portfolio', 'filter' ); ?>
 
 				<div class="article-wrapper wrap">
 				<?php
@@ -46,7 +34,7 @@ get_header(); ?>
 					 * If you want to override this in a child theme, then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
-					// get_template_part( 'template-parts/portfolio', 'archive' );
+					get_template_part( 'template-parts/portfolio', 'archive' );
 
 				endwhile; ?>
 
